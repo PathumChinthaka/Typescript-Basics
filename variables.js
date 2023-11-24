bcc5fe32ed6eef96f1ff9fdc51828199980da1e9
Pathum chinthaka <chinthakapeiris2002@gmail.com>
2023-11-25 00:24:06 +0530

    init

---
+++ variables.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var myName = "Hellow me";
+console.log(myName);
+myName.toLowerCase();
+//Regular function parameter check
+function myFunction(name, id) {
+    console.log("name :" + name + "and id is :" + id);
+}
+myFunction("joy", 123);
+//return type check function
+function check() {
+    return 123;
+}
+console.log(check());
---
+++ variables.ts
@@ -0,0 +1,23 @@
+let myName:string="Hellow me";
+console.log(myName);
+
+//variables
+const city:string="panadura";
+
+const myAge:number=21;
+
+const isPretty:boolean=false;
+
+//Regular function parameter check
+function myFunction(name:string,id:number){
+  console.log("name :"+name+"and id is :"+id);
+}
+myFunction("joy",123);
+
+//return type check function
+function check():number{
+ return 123; 
+}
+
+//just ignore 
+export {};
